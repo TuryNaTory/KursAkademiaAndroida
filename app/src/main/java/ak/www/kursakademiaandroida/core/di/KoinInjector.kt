c106bbe53848bb8e318a8a60101cb0927c117343
Turo <artkubaj@gmail.com>
2020-07-14 18:28:02 +0200

    Added app module and database module.

--- app/src/main/java/ak/www/kursakademiaandroida/core/di/KoinInjector.kt
+++ app/src/main/java/ak/www/kursakademiaandroida/core/di/KoinInjector.kt
@@ -3,5 +3,7 @@ package ak.www.kursakademiaandroida.core.di
 import org.koin.core.module.Module
 
 val koinInjector: List<Module> = listOf(
-    networkModule
+    networkModule,
+    appModule,
+    databaseModule
 )
